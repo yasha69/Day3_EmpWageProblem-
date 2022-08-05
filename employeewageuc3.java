a67c2dcf1f5a400d9193226bbb32957a6e5ce7a2
Yash <yashatkulwar2000@gmail.com>
2022-08-05 21:01:36 +0530

    added part time wage

---
+++ employeewageuc3.java
@@ -0,0 +1,21 @@
+public class employeewageuc3 {
+    public static void main(String[] arg) {
+        int IS_FULL_TIME= 1;
+        int IS_PART_TIME = 2;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int PART_TIME_HOUR = 4;
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_FULL_TIME){
+            int TOTAL_SALARY = (WAGE_PER_HOUR * FULL_DAY_HOUR);
+            System.out.println("TOTAL_SALARY" +TOTAL_SALARY);
+        }
+        else if (empcheck == IS_PART_TIME){
+            int PART_TIME_SALARY = (WAGE_PER_HOUR * PART_TIME_HOUR);
+            System.out.println("PART_TIME_SALARY" +PART_TIME_SALARY);
+        }
+        else{
+            System.out.println("salary is zero");
+        }
+    }
+}
